faffa3473ee61f18fc66cfd7805fb4b0040d0c10
Sampat Ghosh <sampatghosh1995@gmail.com>
2020-06-07 14:28:47 +0530

    Add files via upload

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,17 @@
 module.exports = {
 	rpc: {
 		host:"localhost",
-		port:8543
+		port:8545,
+		gas: 20000000
 	},
 	networks: {
 		development: {
 			host: "localhost", //our network is running on localhost
-			port: 8543, // port where your blockchain is running
+			port: 8545, // port where your blockchain is running
 			network_id: "*",
 			from: "0x7c5c723cd38eeb2d2975dc241fa0a54a9f0ff3da", // use the account-id generated during the setup process
 			gas: 20000000
+
 		}
 	}
 };
